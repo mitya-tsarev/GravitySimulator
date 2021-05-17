04fe0b79527f5205b87d2330de5d868a04ebfb97
Mitya Tsarev <mitya.tsarev@gmail.com>
2021-05-17 17:04:25 +0300

    Add TAU parameter to BH universe

--- include/BarnesHutUniverse.h
+++ include/BarnesHutUniverse.h
@@ -2,10 +2,13 @@
 #define GRAVITYSIMULATOR_BARNESHUTUNIVERSE_H
 
 #include "LinearTimeUniverse.h"
+#include "OctTree.h"
 
 class BarnesHutUniverse : public LinearTimeUniverse {
+    double TAU;
+    OctTree* tree;
 public:
-    explicit BarnesHutUniverse(double dt, int method);
+    explicit BarnesHutUniverse(double dt, double tau, int method);
 
     void updateAccels() override;
 };
