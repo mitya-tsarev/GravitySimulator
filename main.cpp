019a430c035106913c992e15c4a3979a9bfb5ffc
Mitya Tsarev <mitya.tsarev@gmail.com>
2021-05-17 18:46:06 +0300

    Remove useless includes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
-#include <iostream>
-#include <stdexcept>
 #include <fstream>
 #include <string>
-#include <sstream>
 
 #include "include/Simulator.h"
 
 int main() {
-
     std::ifstream inputfile;
     inputfile.open("../input");
     try {
