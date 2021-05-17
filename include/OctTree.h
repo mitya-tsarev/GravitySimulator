ed30160a1aed99032b64b00ed07ce4535a6a595c
Mitya Tsarev <mitya.tsarev@gmail.com>
2021-05-18 02:13:01 +0300

    Minor refactoring

--- include/OctTree.h
+++ include/OctTree.h
@@ -25,27 +25,38 @@ private:
         Node *BSW = nullptr;
 
         double getX();
+
         double getY();
+
         double getZ();
 
         double getSize();
 
         double getXm();
+
         double getYm();
+
         double getZm();
 
         double getSumMass();
 
         Node(const mathing::Vec4 &pos, double size, const mathing::Vec4 &posM, double sumMass);
+
         ~Node();
 
         static Node *
         createOrInsert(Node *n, const mathing::Vec4 &posit, double h, const mathing::Vec4 &posit_m, double m);
+
         bool isLeaf();
+
         mathing::Vec4 get_accel(const mathing::Vec4 &position);
+
         void insertInternal(const mathing::Vec4 &posit_m, double m);
+
         Node *insert(const mathing::Vec4 &posit_m, double m);
+
         mathing::Vec4 calcNode(mathing::Vec4 ab, double l);
+
         mathing::Vec4 calcLeaf(const mathing::Vec4 &position, const mathing::Vec4 &ab, double l);
     };
 
@@ -56,13 +67,20 @@ private:
 public:
 
     OctTree(double minCoord, double maxCoord) : minCoord(minCoord), maxCoord(maxCoord), SMOOTH(0) {}
+
     OctTree(double minCoord, double maxCoord, double smooth) : minCoord(minCoord), maxCoord(maxCoord), SMOOTH(smooth) {}
+
     ~OctTree();
+
     Node *getRoot();
+
     double getSmoothParam();
+
     void setSmooth(double smooth_param);
-    void insert(const std::vector<Body*>& bodies);
-    void insert(const mathing::Vec4& posit_m, double body_mass);
+
+    void insert(const std::vector<Body *> &bodies);
+
+    void insert(const mathing::Vec4 &posit_m, double body_mass);
 
     mathing::Vec4 get_accel(const mathing::Vec4 &position);
 
